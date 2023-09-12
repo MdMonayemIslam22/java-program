fb18bfe52f563a6c3cbc89c1dc7978e2e5eb3f8d
MdMonayemIslam22 <140682306+MdMonayemIslam22@users.noreply.github.com>
2023-09-12 11:44:38 -0700

    program21 added

--- oop-with-java.java
+++ oop-with-java.java
@@ -616,3 +616,42 @@ public class SortEvenOddSeparately {
 // output:
 // Original Array: [5, 2, 9, 1, 5, 6]
 // Sorted Array with Even and Odd Elements Separated: [2, 6, 1, 5, 5, 9]
+
+// 21.Write a Java program to left rotate an array.
+
+class RotateLeft {  
+    public static void main(String[] args) {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        //n determine the number of times an array should be rotated  
+        int n = 3;  
+        //Displays original array  
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        }  
+        //Rotate the given array by n times toward left  
+        for(int i = 0; i < n; i++){  
+            int j, first;  
+            //Stores the first element of the array  
+            first = arr[0];  
+            for(j = 0; j < arr.length-1; j++){  
+                //Shift element of array by one  
+                arr[j] = arr[j+1];  
+            }  
+            //First element of array will be added to the end  
+            arr[j] = first;  
+        }  
+        System.out.println();  
+        //Displays resulting array after rotation  
+        System.out.println("Array after left rotation: ");  
+        for(int i = 0; i< arr.length; i++){  
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
+// output:
+// Original array: 
+// 1 2 3 4 5 
+// Array after left rotation: 
+// 4 5 1 2 3 
